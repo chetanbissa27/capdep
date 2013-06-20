f698156bfa9d175e5ca37eff863318dbaed94ba6
Chetan Bissa <chetan@chetan-Virtualbox.(none)>
2013-06-20 18:36:13 +0530

    try to deploy with capistrano

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapistranoDeploy::Application.config.secret_token = '71446e7cf57e148ac36e20e2af2bf175133b8d65e59c8cd6606f29d61ec0ab999954b514ad934255d3b781d08ad98d78233ed847904abfa3c302f9b9f98f071f'
